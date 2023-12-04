83fc8731ae9430246b345576508681328c3dbbf5
Eternal-nerd <90639066+Eternal-nerd@users.noreply.github.com>
2023-12-04 09:52:59 -0700

    Add files via upload

--- day1/main.cpp
+++ day1/main.cpp
@@ -2,98 +2,105 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <regex>
 #include <unordered_map>
+#include <regex>
 
-bool checkWordNum(const std::string& s) {
-	std::vector<std::string> numWords = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-	bool valid;
-	for (std::string ele : numWords) {
-		valid = true;
-		for (int i=0; i<ele.length(); i++) {
-			if (ele[i]!=s[i]) {
-				valid=false;
-			}
-		}
-		if (valid) {
-			return true;
-		}
-	}
-	return false;
+int combineCharNums(char c1, char c2) {
+    std::string num;
+    num = num + c1 + c2;
+    return stoi(num);
 }
 
-std::string getWordNum(const std::string& s) {
-	std::vector<std::string> numWords = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-	std::unordered_map<std::string, std::string> numWordMap;
-	numWordMap["one"] = "1";
-	numWordMap["two"] = "2";
-	numWordMap["three"] = "3";
-	numWordMap["four"] = "4";
-	numWordMap["five"] = "5";
-	numWordMap["six"] = "6";
-	numWordMap["seven"] = "7";
-	numWordMap["eight"] = "8";
-	numWordMap["nine"] = "9";
+std::string getString(char x) {
+    std::string s(1, x);
+    return s;
+}
 
-	for (std::string ele : numWords) {
-		if (s.find(ele) != std::string::npos) {
-			return numWordMap[ele];
-		}
-	}
+char getNumChar(const std::string& s, int index) {
+    std::unordered_map<std::string, char> number_words;
+    number_words["one"] = '1';
+    number_words["two"] = '2';
+    number_words["three"] = '3';
+    number_words["four"] = '4';
+    number_words["five"] = '5';
+    number_words["six"] = '6';
+    number_words["seven"] = '7';
+    number_words["eight"] = '8';
+    number_words["nine"] = '9';
 
-	return "-1";
 
+    for (auto ele : number_words) {
+        bool found = true;
+        //std::cout << "ele: " << ele.first << std::endl;
+        for (int i = 0; i < ele.first.length(); i++) {
+            if (index + ele.first.length() <= s.length()) {
+                //std::cout << "ele[i]: " << ele.first[i] << " | s[index+i]: " << s[index + i] << "\n";
+                if (ele.first[i] != s[index+i]) {
+                    found = false;
+                    //std::cout << "not it.\n";
+                }
+            }
+            else {
+                found = false;
+            }
+        }
+        if (found) {
+            return ele.second;
+        }
+    }
+
+    return 'x';
 }
 
 int main() {
 
-	std::ifstream file("input.txt");
-	std::string lineString;
-	std::vector<std::string> input;
+    std::ifstream file("input.txt");
+    std::string line;
+    std::vector<std::string> input_vec;
+    while (std::getline(file, line)) {
+        input_vec.push_back(line);
+    }
+    file.close();
 
-	while (getline(file, lineString)) {
-		input.push_back(lineString);
-	}
+    int sum = 0;
+    std::regex pattern("\\d");
 
-	file.close();
+    for (int i = 0; i < input_vec.size(); i++) {
+        char first;
+        char second;
+        bool first_flag = false;
+        for (int j = 0; j < input_vec[i].length(); j++) {
+            char currChar = input_vec[i][j];
+            if (std::regex_match(getString(currChar), pattern)) {
+                if (!first_flag) {
+                    first = currChar;
+                    first_flag = true;
+                }
+                second = currChar;
+            }
+            if (getNumChar(input_vec[i], j) != 'x') {
+                currChar = getNumChar(input_vec[i], j);
+                if (!first_flag) {
+                    first = currChar;
+                    first_flag = true;
+                }
+                second = currChar;
+            }
+            
+        }
+        sum += combineCharNums(first, second);
+    }
 
-	//input = {"1abc2", "pqr3stu8vwx", "afourb2c3d4e5f", "treb7uchet"};
+    std::cout << "SUM: " << sum << "\n";
+    
+    /*std::string test = "fewbbovwov6ixsixn";
 
-	int sum = 0;
-	//std::cout << "running total: " << sum << std::endl;
-	std::regex pattern("\\d");
-	for (auto it = input.begin(); it!=input.end(); it++) {
-		//std::cout << *it << std::endl;
-		std::string s = *it;
-		std::string first, second;
-		bool firstFlag(false);
-		for (int i=0; i<s.length(); i++) {
-			std::string checkChar(1, s[i]);
-			std::string checkWord = s.substr(i, 5);
-			if (std::regex_match(checkChar, pattern)) {
-				if (!firstFlag) {
-					first = checkChar;
-					firstFlag = true;
-				}
-				second = checkChar;
-			}
-			else {
-				if (checkWordNum(checkWord)) {
-					if (!firstFlag) {
-						first = getWordNum(checkWord);
-						firstFlag=true;
-					}
-					second = getWordNum(checkWord);
-				}
-			}
-		}
-		//std::cout << first << second << "\n";
-		sum+=std::stoi(first.append(second));
-		//std::cout << "running total: " << sum << std::endl;
-	}
+    for (int i = 0; i < test.length(); i++) {
+        char check = getNumChar(test, i);
+        if (check != 'x') {
+            std::cout << "at index: " << i << " num: " << check << "\n";
+        }
+    }*/
 
-	std::cout << "SUM: " << sum << std::endl;
-
-	return 0;
-
+    return sum;
 }
